6a2ebd035f105e51d4ab50b1aed4570af87f3d18
juan_cruz <juanbarcruz96@gmail.com>
2020-03-16 22:06:48 -0300

    asd

--- v2.py
+++ v2.py
@@ -9,26 +9,36 @@ class Campeon():
         self.armadura = armadura
         self.ataque_basico = basico
 
-   
+
 
     def mostrar_items(self):
-        print('La lista de items es:',self.lista_items)
+        print('La lista de items de ',self.nombre ,'es:',self.lista_items)
 
     def agregar_item(self,item):
         bolso_lleno = True
         for i in range(0,5):
             if(self.lista_items[i]=='0'):
                 self.lista_items[i] = item
-                bolso_lleno = False    
-                self.mostrar_items()            
+                bolso_lleno = False
+                self.mostrar_items()
                 break
 
         if(bolso_lleno):
-            print('El inventario esta lleno, tiene que vender un item: ')
+            print('\n[!]El inventario esta lleno, tiene que vender un item: ')
             self.mostrar_items()
             item_vender = input('Indique posicion del item (1-6)>> ')
             self.lista_items[int(item_vender)-1] = '0'
+            print("[+] Se vendio el item :)")
             self.agregar_item(item)
+
+    def eliminar_item(item):
+        for i in range(0,5):
+            if(self.lista_items[i]==item):
+                self.lista_items[i] = '0'
+                #ganar oro
+    def eliminar_item(posicion):
+        self.lista_items[posicion-1] = '0'
+
 
 
 
@@ -41,18 +51,35 @@ def help():
 
 
 def main():
+
+    campeones = ['','']
+    index_champs = 0
+    lista_index = {}
+
+
     entrada = input('>> ')
-
-    if(entrada.find('-c')):
+    while(entrada!="exit()"):
         secuencia = entrada.split(" ")
-        print('Agregando champ',secuencia[3])
-        #Add -c <champ> <vida> <energia/mana> <poder_habilidad> <armadura> <danio_basico>
-        champ = Campeon(secuencia[2],secuencia[3],secuencia[4],secuencia[5],secuencia[6],secuencia[7])
-    #lista de campeones??
+        secuencia.append('/x')
 
-    if(entrada.find(-i)):
-        secuencia = entrada.split(" ")
-        champ.agregar_item(secuencia[2])
+        if(secuencia[1]=='-c'):
+
+            print('Agregando champ',secuencia[2])
+            #Add -c <champ> <vida> <energia/mana> <poder_habilidad> <armadura> <danio_basico>
+            champ = Campeon(secuencia[2],secuencia[3],secuencia[4],secuencia[5],secuencia[6],secuencia[7])
+            campeones[index_champs] = champ
+            lista_index.setdefault(secuencia[2],index_champs)
+            index_champs += 1
+
+
+        if(secuencia[1]=='-i'):
+            # add -i <champ_index/name> <item>
+            campeones[int(secuencia[2])].agregar_item(secuencia[3])
+
+        if(secuencia[0]=='show'):
+            print(lista_index)
+
+        entrada = input('>> ')
 
 
 main()
